d1d524b89ecf3237b0d38a7374267555c36e7142
Josephmusngi21 <josephmusngi7@gmail.com>
2024-06-15 23:14:18 -0700

    created object for bird

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+let board;
+let boardWidth;
+let boardHeight;
+let context;
+
+//Bird
+let birdWidth;
+let birdHeight;
+let birdX = 50;
+let birdY = boardHeight - birdHeight;
+let birdImg;
+
+//class/object for bird
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    heigh : birdHeight 
+}
