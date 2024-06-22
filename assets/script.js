910208e06a9aa92f318df5b0d3eaa83b2c53c4b5
Josephmusngi21 <josephmusngi7@gmail.com>
2024-06-22 13:33:40 -0700

    updates script.js with board

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,22 @@ let birdHeight;
 let birdX = 50;
 let birdY = boardHeight - birdHeight;
 let birdImg;
-
+let birdColor = 'blue';
 //class/object for bird
 let bird = {
     x : birdX,
     y : birdY,
     width : birdWidth,
     heigh : birdHeight 
+}
+
+//create board
+window.onload = () => {
+    board = document.getElementById("board");
+    board.height;
+    board.width;
+    context = board.getContext("2d");
+
+    birdImg = new Image();
+    birdImg.src = `,/assets/sprites/${birdColor}bird-midflap.png`;
 }
