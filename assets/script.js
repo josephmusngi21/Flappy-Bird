22348cc90b95d487a14143913d3cab68b8b22112
Josephmusngi21 <josephmusngi7@gmail.com>
2024-06-25 12:03:10 -0700

    addedpipe info, physics and updated function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 let board;
-let boardWidth;
-let boardHeight;
+let boardWidth = 800;
+let boardHeight = 660;
 let context;
 
+
 //Bird
-let birdWidth;
-let birdHeight;
+let birdWidth = 17;
+let birdHeight = 12;
 let birdX = 50;
 let birdY = boardHeight - birdHeight;
 let birdImg;
@@ -15,16 +16,40 @@ let bird = {
     x : birdX,
     y : birdY,
     width : birdWidth,
-    heigh : birdHeight 
+    height : birdHeight 
 }
 
+let pipeArray = [];
+let pipe1Width;
+let pipe2Width;
+let pipe3Width;
+
+let pipeHeight;
+let pipeX;
+let pipeY = boardHeight - birdHeight;
+
+let pipe1Img;
+let pipe2Img;
+let pipe3Img;
+
+//physics
+let velocityX = -8;
+let velocityY= 0;
+let gravity = 0.6;
+
+let gameOver = false;
+let score = 0;
+
+
 //create board
-window.onload = () => {
+window.onload = function() {
     board = document.getElementById("board");
-    board.height;
-    board.width;
+    board.height = boardHeight;
+    board.width = boardWidth;
     context = board.getContext("2d");
 
     birdImg = new Image();
     birdImg.src = `,/assets/sprites/${birdColor}bird-midflap.png`;
+    birdImg.onload = () => context.drawImage(birdImg, bird.x, bird.y, bird.width. bird.height);
+
 }
